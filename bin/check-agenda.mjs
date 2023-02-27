0410003865f2c9e77b8f041bbf82fac6d6e2d639
Pascal Brand <pascal.brand38@gmail.com>
2023-02-27 17:47:40 +0100

    bin/check-agenda.mjs: check compta alone
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- bin/check-agenda.mjs
+++ bin/check-agenda.mjs
@@ -22,7 +22,8 @@ function serialToStr(serial) {
   l = Math.floor(j / 11);
   let nMonth = j + 2 - (12 * l);
   let nYear = 100 * (n - 49) + i + l;
-  return nDay + '/' + nMonth + '/' + nYear;
+  // return nDay + '/' + nMonth + '/' + nYear;
+  return nYear + '/' + nMonth + '/' + nDay;
 }
 
 // read an xls or ods file
@@ -30,7 +31,7 @@ function serialToStr(serial) {
 // data, colName is the col that contains the name of the cat (typically 'A'), 
 // colArrival and colDeparture are the cat's booking
 // it returns a json with object { name, arrival, departure }
-function readXls(name, sheetName, colName, colArrival, colDeparture) {
+function readXls(name, sheetName, colName, colArrival, colDeparture, colStatusPay1=null, colStatusPay2=null, colStatusPay3=null) {
   const file = reader.readFile(name)
   let data = []
   // { header: "A" } indicates the json keys are A, B, C,... (cf. https://docs.sheetjs.com/docs/api/utilities/)
@@ -39,6 +40,7 @@ function readXls(name, sheetName, colName, colArrival, colDeparture) {
       name: res[colName],
       arrival: Math.floor(res[colArrival]),
       departure: Math.floor(res[colDeparture]),
+      statusPay: (colStatusPay1 ? [ res[colStatusPay1], res[colStatusPay2], res[colStatusPay3] ] : null),
     })
   })
   data = data.filter(e => (e.name !== undefined) && !isNaN(e.arrival) && !isNaN(e.departure))
@@ -74,8 +76,7 @@ function checkDates(dataCompta, dataAgenda) {
     const compta = dataCompta.filter(e => e[what] === d.date)
     const agenda = dataAgenda.filter(e => e[what] === d.date)
     if (compta.length !== agenda.length) {
-      console.log('---------------------------------')
-      console.log(`${what} on ${serialToStr(d.date)}`)
+      console.log(`--- ${what} on ${serialToStr(d.date)} ----------------`)
       console.log('Compta: ')
       compta.forEach(c => console.log(`   ${c.name}`))
       console.log('Agenda: ')
@@ -84,12 +85,47 @@ function checkDates(dataCompta, dataAgenda) {
   })
 }
 
+function checkStatusPay(dataCompta) {
+  const epochToday = Date.now();
+
+  console.log('-------------------------------------------------')
+  console.log('------------------------------------------ COMPTA')
+  console.log('-------------------------------------------------')
+  dataCompta.forEach(data => {
+    const departureStr = serialToStr(data.departure)
+    const epochDeparture = Date.parse(departureStr)
+    const epochDeparture10 = epochDeparture - 1000*60*60*24
+    const epochDeparture20 = epochDeparture10 - 1000*60*60*24
+    if (epochDeparture < epochToday) {
+      //console.log(data.name)
+      data.statusPay.forEach(status => {
+        switch (status) {
+          case 'Attente':
+            console.log(`*** ATTENTE  ${departureStr} ${data.name}`)
+            break
+          case 'Reçu':
+            if (epochDeparture10 < epochToday) {
+              console.log(`    Reçu     ${departureStr} ${data.name}`)
+            }
+            break
+          case 'Envoyé':
+            if (epochDeparture20 < epochToday) {
+              console.log(`    Envoyé   ${departureStr} ${data.name}`)
+            }
+            break
+          }
+      })
+    }
+  })
+}
+
+
 function main() {
   const argv = process.argv
   // console.log(argv)
 
   // Reading compta and agenda data
-  let dataCompta = readXls(argv[2], 'Compta', 'B', 'W', 'X')
+  let dataCompta = readXls(argv[2], 'Compta', 'B', 'W', 'X', 'K', 'O', 'S')
   let dataAgenda = readXls(argv[3], 'Résa', 'A', 'I', 'K')
 
   // filter the dates from the compta that are prior the 1st arrival in the agenda
@@ -98,6 +134,7 @@ function main() {
 
   // check coherency
   checkDates(dataCompta, dataAgenda)
+  checkStatusPay(dataCompta)
 }
 
 main();
