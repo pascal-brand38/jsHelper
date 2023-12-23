f3e1bdb0289d04ed4f31af7f581e2d41893a2027
Pascal Brand <pascal.brand38@gmail.com>
2023-12-23 19:52:32 +0100

    bin/analyze-apache-logs.mjs: code factoring 3
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- bin/analyze-apache-logs/antispam-stopforumspam.mjs
+++ bin/analyze-apache-logs/antispam-stopforumspam.mjs
@@ -3,33 +3,43 @@
 
 import fetch from 'node-fetch'
 
-async function populateDbIp(apacheData, dbIp) {
-  if (dbIp === undefined) {
-    return undefined
-  }
-  let uniqueIps = new Set(apacheData.map(function(a) {return a.remoteHost;}));
+// data is the apache data from logs - see apache-data.mjs
+// db is the ip database - see db-ip.mjs
+
+// return stopforumspam data related to ips in data
+async function _read(data) {
+  let uniqueIps = new Set(data.map(function(a) {return a.remoteHost;}));
   if (uniqueIps.size === 0) {
-    return
+    return undefined
   }
   if (uniqueIps.size === 1) {
     uniqueIps.push('0.0.0.0')   // to have a list of results
   }
+
   const url = 'https://api.stopforumspam.org/api?json&ip=' + [...uniqueIps].join('&ip=')
-  let results
   try {
     const response = await fetch(url);
-    results = await response.json();
-    console.log(results)
+    return await response.json();
   } catch {
     return undefined
   }
+}
 
-  //
-  results.ip.forEach(element => {
-    let dbIpItem = dbIp[element.value]
+// return updated db with data from stopforumspam.org
+async function populateDbIp(data, db) {
+  if (db === undefined) {
+    return db
+  }
+  const spamdata = await _read(data)
+  if (spamdata === undefined) {
+    return db
+  }
+
+  spamdata.ip.forEach(element => {
+    let dbIpItem = db[element.value]
     if (dbIpItem === undefined) {
       // create the entry for this ip
-      dbIp[element.value] = {
+      db[element.value] = {
         epoch: 0,
       }
       console.log(`${element.value} NOT FOUND`)
@@ -38,7 +48,7 @@ async function populateDbIp(apacheData, dbIp) {
     }
   });
 
-  return dbIp
+  return db
 }
 // https://api.stopforumspam.org/api?json&ip=0.0.0.0&ip=85.68.121.4
 
