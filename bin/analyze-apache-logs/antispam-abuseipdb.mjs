04a19c288f0dd1aec3166e6fa63ac7ad7a5d091b
Pascal Brand <pascal.brand38@gmail.com>
2023-12-25 18:28:02 +0100

    abuseipdb
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- bin/analyze-apache-logs/antispam-abuseipdb.mjs
+++ bin/analyze-apache-logs/antispam-abuseipdb.mjs
@@ -7,29 +7,33 @@ import fs from 'fs'
 // https://docs.abuseipdb.com/#blacklist-endpoint
 
 // return stopforumspam data related to ips in data
-/*
 async function getBlacklist() {
   const headers = {
     'Accept': 'application/json',
     'Key': 'XXX',   // remove this hardcoded value
-    'confidenceMinimum':'90',
   }
-  const querystring = {
-    'confidenceMinimum':'90'
-  }
-  const url = 'https://api.abuseipdb.com/api/v2/blacklist?confidenceMinimum=30'
+  const params = new URLSearchParams();
+  params.append('confidenceMinimum', 90);
+
+  const url = 'https://api.abuseipdb.com/api/v2/blacklist'
   let rawDatas = {}
   try {
-    const response = await fetch(url, {headers: headers});
+    const response = await fetch(url, { headers: headers, body: params });
     rawDatas = await response.json();
   } catch {
+    console.log('abuseipdb RETURNED AN ERROR')
+    return {}
   }
-  return rawDatas
-}
-*/
 
-async function getBlacklist() {
-  const rawDatas = await JSON.parse(fs.readFileSync('C:\\Users\\pasca\\Desktop\\blacklist.txt', 'utf8'))
+  // save it just in case
+  fs.writeFileSync('C:\\tmp\\blacklist.txt', JSON.stringify(rawDatas))
+
+  if (rawDatas.errors !== undefined) {
+    console.log('CANNOT READ abuseipdb RESULT')
+    console.log(rawDatas)
+    return {}
+  }
+
   let result = []
   rawDatas.data.forEach(element => {
     result[element.ipAddress] = element
@@ -37,40 +41,18 @@ async function getBlacklist() {
   return result
 }
 
-// jsonObject is the returned info from stopforumspam.org, typically
-// {
-//   value: '104.168.153.70',
-//   appears: 1,
-//   frequency: 17,
-//   lastseen: '2023-12-17 10:39:57',
-//   delegated: 'us',
-//   asn: 54290,
-//   country: 'us',
-//   confidence: 79.07
+// async function getBlacklist() {
+//   const rawDatas = await JSON.parse(fs.readFileSync('C:\\Users\\pasca\\Desktop\\blacklist.txt', 'utf8'))
+//   let result = []
+//   rawDatas.data.forEach(element => {
+//     result[element.ipAddress] = element
+//   });
+//   return result
 // }
 
-// TODO: return a status about unknown, crawler, spam...
-// function ipStatus(jsonObject) {
-//   if (jsonObject === undefined) {
-//     console.log(jsonObject)
-//     return true
-//   }
-//   if ((jsonObject.appears > 0) && (jsonObject.confidence > 0)) {
-//     // TODO: use a confidence threshold
-//     // TODO: use lastseen
-//     return false
-//   } else {
-//     return true
-//   }
-// }
-
-// https://api.stopforumspam.org/api?json&ip=0.0.0.0&ip=85.68.121.4
 
 function ipStatus(jsonObject) {
-  if (jsonObject === undefined) {
-    return true
-  }
-  return false
+  return !((jsonObject) && (jsonObject.abuseConfidenceScore !== undefined) && (jsonObject.abuseConfidenceScore>=30))
 }
 
 export default {
