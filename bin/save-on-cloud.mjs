da51006e276f0f96481689f31b43b4b35a6b840f
Pascal Brand <pascal.brand38@gmail.com>
2024-08-19 18:23:07 +0200

    bin/save-on-cloud.mjs: no duplicate in .save-with-dates
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- bin/save-on-cloud.mjs
+++ bin/save-on-cloud.mjs
@@ -13,6 +13,9 @@
 import path from 'path'
 import fs from 'fs'
 import fileSyncCmp from 'file-sync-cmp'
+import _yargs from 'yargs'
+import { hideBin } from 'yargs/helpers';
+
 
 let statistics = {
   identical: 0,
@@ -21,16 +24,59 @@ let statistics = {
 }
 
 let _dryrun = false
-let _dirDated = 'save-with-dates'
+let _dirDated = '.save-with-dates'
+
+async function getArgs(usage) {
+  console.log(process.argv)
+  const yargs = _yargs(hideBin(process.argv));
+
+  let options = yargs
+    .usage(usage)
+    .help('help').alias('help', 'h')
+    .version('version', '1.0').alias('version', 'V')
+    .options({
+      "src-dir": {
+        description: "source directory to copy",
+        requiresArg: true,
+        required: true,
+      },
+      "dst-dir": {
+        description: "dst directory to copy",
+        requiresArg: true,
+        required: true,
+      },
+    })
+    // .fail((msg, err, yargs) => {
+    //   if (err) throw err // preserve stack
+    //   console.error('You broke it!')
+    //   console.error(msg)
+    //   console.error('You should be doing', yargs.help())
+    //   throw 'YARGS ERROR'
+    // })
+    .argv;
+
+  options.excludeDirs = [ 'tmp', '.tmp.drivedownload' ]
+
+  return options
+}
+
+
 
 /// get the list of all files in rootDir, from subDir recursiveley
-function walkDir(srcFiles, rootDir, subDir) {
+function walkDir(options, srcFiles, rootDir, subDir) {
+  if (subDir === '') {
+    console.log('--- walkDir')
+  }
+  console.log(`walkdir ${subDir}`)
   const thisDir = path.join(rootDir, subDir);
   fs.readdirSync(thisDir).forEach(file => {
     const absolute = path.join(thisDir, file);
     const sub = path.join(subDir, file)
     if (fs.statSync(absolute).isDirectory()) {
-      walkDir(srcFiles, rootDir, sub);
+      if (options.excludeDirs.includes(file)) {
+        return
+      }
+      walkDir(options, srcFiles, rootDir, sub);
     } else {
       srcFiles.push(sub);
     }
@@ -38,7 +84,7 @@ function walkDir(srcFiles, rootDir, subDir) {
 }
 
 
-function main() {
+function main(options) {
   const argv = process.argv
   // console.log(argv)
 
@@ -46,11 +92,13 @@ function main() {
   let dateExt = '' + today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + ("0" + today.getDate()).slice(-2)
 
   // Reading compta and agenda data
-  let srcDir = argv[2]
-  let dstDir = argv[3]
+  console.log(options)
+  console.log(options.srcDir)
+  let srcDir = options.srcDir
+  let dstDir = options.dstDir
 
   let srcFiles = []
-  walkDir(srcFiles, srcDir, '')
+  walkDir(options, srcFiles, srcDir, '')
   // console.log(srcFiles)
   srcFiles.forEach(file => {
     const srcFile = path.join(srcDir, file);
@@ -93,35 +141,27 @@ function main() {
     }
 
     if (mustCopy) {
-      let fileExt = dstFile.split('.').pop();
-      const dstDatedFile = path.join(dstDir, _dirDated, file) + '.' + dateExt + '.' + fileExt
-
-      if (_dryrun) {
-        if (update) {
-          console.log(`fs.copyFileSync(${srcFile}, ${dstDatedFile})`)
+      if (update) {
+        const subdir = path.dirname(file)
+        const stat = fs.statSync(dstFile)
+        const fileParse = path.parse(dstFile)
+        const dstDatedFile = path.join(dstDir, _dirDated, subdir, fileParse.name) + '-' + stat.mtime.toISOString().replaceAll(':', '-') + fileParse.ext
+        // console.log(dstDatedFile)
+        // throw('STOP')
+        const dirname = path.dirname(dstDatedFile)
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname, { recursive: true });
         }
-        console.log(`fs.copyFileSync(${srcFile}, ${dstFile})`)
-      } else {
-        if (update) {
-          const dirname = path.dirname(dstDatedFile)
-          if (!fs.existsSync(dirname)) {
-            fs.mkdirSync(dirname, { recursive: true });
-          }
-          fs.copyFileSync(dstFile, dstDatedFile)
-        }
-        fs.copyFileSync(srcFile, dstFile)
+        fs.copyFileSync(dstFile, dstDatedFile)
       }
-      console.log(`Copy ${path.basename(srcFile)}`)
+      fs.copyFileSync(srcFile, dstFile)
+      console.log(`${update ? 'Update' : 'Copy  '} ${path.basename(srcFile)}`)
     }
   })
 }
 
-
-if (process.argv.length !== 4) {
-  console.log(`save-on-cloud /c/Users/pasca/Desktop/save-on-cloud "/c/Users/pasca/Mon Drive/tmp/save-on-cloud"`)
-} else {
-  main();
-  console.log('-------------------------------')
-  console.log(statistics)
-  console.log('Done')
-}
+const options = await getArgs(`save-on-cloud --src-dir=/c/Users/pasca/Desktop/save-on-cloud --dst-dir="/c/Users/pasca/Mon Drive/tmp/save-on-cloud"`)
+main(options);
+console.log('-------------------------------')
+console.log(statistics)
+console.log('Done')
